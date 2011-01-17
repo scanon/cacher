2eda9f5b2af149028c78356c1a2a5cfd13f1c285
Shane Canon <scanon@gmail.com>
2011-01-16 22:04:12 -0800

    Added debugging, timing, and removed read_status completely.

--- cacher.c
+++ cacher.c
@@ -9,21 +9,15 @@
 #define _GNU_SOURCE
 #include <string.h>
 #include <signal.h>
+#include <time.h>
  
 #define SHM_KEY 1098
  
 void *cache_file(char *file,int key);
 void cleanup();
+void status();
 
-struct cache_file {
-  void *ptr; 
-  int id;
-  struct cache_file *next;
-};
-
-struct cache_file *head;
-struct cache_file *tail;
-
+int debug=0;
 
 int main(int argc, char *argv[])
 {
@@ -31,12 +25,18 @@ int main(int argc, char *argv[])
    int key;
    char *name;
 
+   if (getenv("DEBUG_CACHER")!=NULL)
+     debug=1;
+   if (argc==1){
+     status();
+     return 0;
+   }
    if (argv[1][0]=='-'){
      cleanup();
      return 0;
    }
    for (key=SHM_KEY,i=1;i<argc;i++,key++){
-    cache_file(argv[i],key); 
+      cache_file(argv[i],key); 
    }
    return 0;
 }
@@ -49,9 +49,10 @@ void *cache_file(char *file,int key)
    int fd;
    int br;
    struct stat st;
-   struct cache_file *c;
+   time_t start;
 
 
+   start=time(NULL);   
    fd=open(file,O_RDONLY);
    fstat(fd,&st);
    size=st.st_size;
@@ -69,18 +70,8 @@ void *cache_file(char *file,int key)
    strcpy(ptr,file);
    ptr+=strlen(file)+1;
    br=read(fd,ptr,size);
-   printf("bytes read: %ld\n",br);
-//   fprintf(f,"%d:%s:%ld:%d:%d\n",key,file,ptr,shmid,size);
-   c=malloc(sizeof(struct cache_file));
-   c->id=shmid;
-   c->ptr=ptr;
-   if (head==NULL){
-     head=c;
-   }
-   else{
-     tail->next=c;
-   }
-   tail=c;
+   if (debug)
+     fprintf(stderr,"%s: cached %ld bytes in %d seconds\n",file,br,time(NULL)-start);
 
    return ptr;
 }
@@ -90,13 +81,45 @@ void cleanup()
   struct shm_info si;
   struct shmid_ds ss;
   int i,j;
-  printf("Cleanup\n");  
+  if (debug)
+    fprintf(stderr,"Cleanup\n");  
 
   shmctl(0,SHM_INFO,(struct shmid_ds *)&si);
   for (i=0;i<si.used_ids;i++){
     j=shmctl(i,SHM_STAT,&ss);
     if (j>0){
       shmctl(j, IPC_RMID, NULL);
+    } 
+  }
+}
+
+void status()
+{
+  struct shm_info si;
+  struct shmid_ds ss;
+  int i,j;
+  char *ptr;
+  key_t key;
+  size_t size;
+  int shm_id;
+
+  shmctl(0,SHM_INFO,(struct shmid_ds *)&si);
+  for (i=0;i<si.used_ids;i++){
+    j=shmctl(i,SHM_STAT,&ss);
+    if (j>0){
+     key=ss.shm_perm.__key;
+     size=ss.shm_segsz;   
+     shm_id = shmget(key, size, 0666);
+     if (shm_id < 0) {
+        fprintf(stderr,"*** shmget error cacher ***\n");
+        exit(1);
+     }
+     ptr = (void *) shmat(j, NULL, 0);
+     if ((long) ptr == -1) { /* attach */
+          fprintf(stderr,"*** shmat error cacher ***\n");
+          exit(1);
+     }
+      printf("0x%08x: %s\n",ss.shm_perm.__key,ptr);
     } 
   }
 }
--- trap_so.c
+++ trap_so.c
@@ -9,7 +9,14 @@
 #include <sys/mman.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include "read_status.h"
+
+struct cache_file_t {
+  char *filename;
+  key_t key;
+  int id;   
+  size_t size;
+  void *ptr;
+};
 
 void * (*next_mmap)(void *start, size_t length, int prot, int flags, int fd, off_t offset);
 void * (*next_mmap64)(void *__addr, size_t __len, int __prot, int __flags, int __fd, __off64_t __offset);
@@ -20,14 +27,16 @@ int check_ptr(void *ptr);
 
 #define MAX_CACHE 20
 struct cache_file_t cf[MAX_CACHE];
+int debug=0;
 
 void * mmap(void *start, size_t length, int prot, int flags, int fd, off_t offset)
 {
   void *p=NULL;
 
-//  fprintf(stderr,"mmap %lx %ld %ld %d\n",(long)start,length,offset,prot);
   if (next_mmap == NULL)
     init();
+  if (debug)
+    fprintf(stderr,"trapped mmap: start=0x%lx length=%ld %ld\n",(long)start,length,offset);
   if (prot==PROT_READ){
     p=lookup_file(fd,cf);
   }
@@ -41,7 +50,8 @@ void * mmap64(void *start, size_t length, int prot, int flags, int fd, __off64_t
 {
   void *p=NULL;
 
-//  fprintf(stderr,"mmap64 %lx %ld %ld %d\n",(long)start,length,offset,prot);
+  if (debug)
+    fprintf(stderr,"trapped mmap64: start=0x%lx %ld %ld %d\n",(long)start,length,offset,prot);
   if (next_mmap64 == NULL)
     init();
   if (prot==PROT_READ){
@@ -54,11 +64,13 @@ void * mmap64(void *start, size_t length, int prot, int flags, int fd, __off64_t
 }
 int munmap(void *start, size_t length)
 {
-//  fprintf(stderr,"munmap %lx %ld\n",(long)start,length);
+  if (debug)
+    fprintf(stderr,"trapped munmap: 0x%lx %ld\n",(long)start,length);
   if (next_mmap64 == NULL)
     init();
   if (check_ptr(start)){
-//    fprintf(stderr,"DEBUG: Skipping %lx\n",start);
+  if (debug)
+      fprintf(stderr,"DEBUG: Skipping %lx\n",start);
     return 1;
   }
   else
@@ -73,14 +85,16 @@ void init(void)
   struct shmid_ds ss;
   int i,j,k=0;
   void *ptr;
- 
+
+  if (getenv("DEBUG_CACHER")!=NULL)
+    debug=1; 
   shmctl(0,SHM_INFO,(struct shmid_ds *)&si);
   for (k=0;k<si.used_ids;k++){
     j=shmctl(k,SHM_STAT,&ss);
     if (j>0){
-     printf("%d\n",j);
      cf[i].key=ss.shm_perm.__key;
-     shm_id = shmget(cf[i].key, cf[i].size, 0666);
+     cf[i].size=ss.shm_segsz;
+     shm_id = shmget(cf[i].key, ss.shm_segsz, 0666);
      if (shm_id < 0) {
         fprintf(stderr,"*** shmget error cacher ***\n");
         exit(1);
@@ -95,11 +109,6 @@ void init(void)
     i++;
     }
   }
-/*
-  while (i<MAX_CACHE && cf[i].filename!=NULL){
-     }
-//     fprintf(stderr,"ptr=%lx\n",(long)cf[i].ptr);
-  }*/
   next_mmap64 = dlsym(RTLD_NEXT, "mmap64");
   next_mmap = dlsym(RTLD_NEXT, "mmap");
   next_munmap = dlsym(RTLD_NEXT, "munmap");
@@ -121,7 +130,8 @@ void * lookup_file(int fd,struct cache_file_t *cf)
   i=0;
   while (i<MAX_CACHE && cf[i].filename!=NULL){
    if (strstr(buff2,cf[i].filename)!=0){
-//     fprintf(stderr,"found ptr 0x%lx\n",(long)cf[i].ptr);
+     if (debug)
+       fprintf(stderr,"found ptr 0x%lx\n",(long)cf[i].ptr);
      return cf[i].ptr;
    } 
    i++;
