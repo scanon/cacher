697ff739fa5f49bef975ad1f8c4cce251c4466bf
Shane Canon <canon@cvrsvc02.(none)>
2011-01-19 22:49:38 -0800

    Additional timing. Chunked reads.

--- cacher.c
+++ cacher.c
@@ -12,8 +12,9 @@
 #include <time.h>
  
 #define SHM_KEY 1098
+#define CHUNK 1048576L
  
-void *cache_file(char *file,int key);
+size_t cache_file(char *file,int key);
 void cleanup();
 void status();
 
@@ -24,7 +25,10 @@ int main(int argc, char *argv[])
    int i;
    int key;
    char *name;
+   time_t start;
+   size_t total=0;
 
+   start=time(NULL);
    if (getenv("DEBUG_CACHER")!=NULL)
      debug=1;
    if (argc==1){
@@ -36,18 +40,21 @@ int main(int argc, char *argv[])
      return 0;
    }
    for (key=SHM_KEY,i=1;i<argc;i++,key++){
-      cache_file(argv[i],key); 
+      total+=cache_file(argv[i],key); 
    }
+   if (debug)
+     fprintf(stderr,"Total: %ld bytes in %ld seconds (%7.2f MB/s)\n",total,time(NULL)-start,(double)total/(double)(1024.0*1024.0*(time(NULL)-start)));
    return 0;
 }
 
-void *cache_file(char *file,int key)
+size_t cache_file(char *file,int key)
 {
    int shmid;
    void *ptr;
    int size;
    int fd;
-   int br;
+   size_t br;
+   size_t total;
    struct stat st;
    time_t start;
 
@@ -56,24 +63,29 @@ void *cache_file(char *file,int key)
    fd=open(file,O_RDONLY);
    fstat(fd,&st);
    size=st.st_size;
-   shmid=shmget(key, size+strlen(file)+1, IPC_CREAT|0666);
+   shmid=shmget(key, size+strlen(file)+1, IPC_CREAT|0600);
    if( shmid == -1){
       perror("shmget");
-      return NULL;
+      return 0;
     }
    ptr = (char*)shmat(shmid, 0, 0);
    if((long)ptr == -1L) {
       shmctl(shmid, IPC_RMID, NULL);
       perror("shmget");
-      return NULL;
+      return 0;
    }
    strcpy(ptr,file);
    ptr+=strlen(file)+1;
-   br=read(fd,ptr,size);
+   total=0;
+   while( (br=read(fd,ptr,CHUNK))>0){
+     ptr+=br;
+     total+=br;
+   }
+   shmdt(ptr);
    if (debug)
-     fprintf(stderr,"%s: cached %ld bytes in %d seconds\n",file,br,time(NULL)-start);
+     fprintf(stderr,"%-70s: cached %10ld bytes in %3d seconds\n",file,total,time(NULL)-start);
 
-   return ptr;
+   return total;
 }
 
 void cleanup()
@@ -106,10 +118,10 @@ void status()
   shmctl(0,SHM_INFO,(struct shmid_ds *)&si);
   for (i=0;i<si.used_ids;i++){
     j=shmctl(i,SHM_STAT,&ss);
-    if (j>0){
+    if (j>0 && ss.shm_perm.uid==getuid()){
      key=ss.shm_perm.__key;
      size=ss.shm_segsz;   
-     shm_id = shmget(key, size, 0666);
+     shm_id = shmget(key, size, 0600);
      if (shm_id < 0) {
         fprintf(stderr,"*** shmget error cacher ***\n");
         exit(1);
